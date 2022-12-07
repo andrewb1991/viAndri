dc013c373ff36b46961ace7924e67087b5985c21
andrewb1991 <andrea.bramucci@gmail.com>
2022-12-07 18:10:24 +0100

    712

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 const elem = document.getElementsByClassName("viaggi");
 console.log(elem);
-const color = document.getElementById(`total`).style.color = "blue";
-const tot = document.getElementById(`total`).innerHTML = "I viaggi in questa pagina sono " + parseInt(elem.length);  
+const color = document.getElementById(`total`).style.color = "red";
+const tot = document.getElementById(`total`).innerHTML = `I viaggi in questa pagina sono   ${elem.length}`;  
 
 
 const hot = document.getElementById("welcomesummer");
