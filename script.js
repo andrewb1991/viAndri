3e8e71597dbecd796444cdbb466e581af4af49a8
andrewb1991 <andrea.bramucci@gmail.com>
2022-12-07 14:25:31 +0100

    7 12 solo

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const elem = document.getElementsByClassName("viaggi");
+console.log(elem);
+const color = document.getElementById(`total`).style.color = "blue";
+const tot = document.getElementById(`total`).innerHTML = "I viaggi in questa pagina sono " + parseInt(elem.length);  
+
+
+const hot = document.getElementById("welcomesummer");
+console.log(hot)
+hot.appendChild(document.createElement("span"));
+
+  
